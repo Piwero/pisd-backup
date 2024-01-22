c8c89ecbff1d619e6b2ff0becf39fa2caa10fab9
Piwero <piwero@gmail.com>
2024-01-22 17:49:19 +0000

    Add command to sudoers on setup

--- src/app.py
+++ src/app.py
@@ -1,17 +1,28 @@
 import os
 import datetime
 
-
 def info_log(message):
     print(f"[INFO] {message}")
-
 
 def warning_log(message):
     print(f"[WARNING] {message}")
 
+def add_to_sudoers():
+    info_log("Configuring sudoers file...")
+
+    # Run the add_to_sudoers.sh script with sudo
+    exit_code = os.system("sudo bash add_to_sudoers.sh")
+
+    if exit_code == 0:
+        info_log("Sudoers file configured successfully.")
+    else:
+        warning_log("Failed to configure sudoers file.")
 
 def install_dependencies():
     info_log("Installing dependencies...")
+
+    # Add user to sudoers
+    add_to_sudoers()
 
     # Install cifs-utils
     exit_code = os.system("sudo apt-get install cifs-utils")
@@ -37,8 +48,7 @@ def install_dependencies():
     if exit_code == 0:
         info_log(f"Downloaded pishrink.sh and moved it to {pishrink_path}.")
     else:
-        warning_log(f"Failed to download or move pishrink.sh.")
-
+        warning_log("Failed to download or move pishrink.sh.")
 
 def backup_raspberry_pi():
     info_log("Backing up Raspberry Pi...")
@@ -53,7 +63,7 @@ def backup_raspberry_pi():
     if exit_code == 0:
         info_log(f"Created backup: {backup_path}")
     else:
-        warning_log(f"Failed to create backup.")
+        warning_log("Failed to create backup.")
 
     # Run pishrink.sh on the created backup
     exit_code = os.system(f"sudo pishrink.sh {backup_path}")
@@ -62,7 +72,6 @@ def backup_raspberry_pi():
         info_log("Ran pishrink.sh on the backup.")
     else:
         warning_log("Failed to run pishrink.sh.")
-
 
 def main():
     while True:
@@ -81,7 +90,6 @@ def main():
             break
         else:
             print("Invalid choice. Please try again.")
-
 
 if __name__ == "__main__":
     main()
---
+++ src/scripts/add_sudoers_privileges.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ensure the script is run with sudo
+if [ "$EUID" -ne 0 ]; then
+    echo "Please run the script with sudo."
+    exit 1
+fi
+
+# Get the current username
+current_user=$SUDO_USER
+
+# Check if the lines are already present in the sudoers file
+if grep -Fxq "$current_user ALL=(ALL) NOPASSWD: /bin/dd bs=4M if=/dev/mmcblk0 of=/home/$current_user/backup-raspis/*" /etc/sudoers && \
+   grep -Fxq "$current_user ALL=(ALL) NOPASSWD: /usr/local/bin/pishrink.sh /home/$current_user/backup-raspis/*" /etc/sudoers; then
+    echo "Lines are already present in sudoers file. No changes needed."
+else
+    # Add the lines to the sudoers file
+    echo "$current_user ALL=(ALL) NOPASSWD: /bin/dd bs=4M if=/dev/mmcblk0 of=/home/$current_user/backup-raspis/*" >> /etc/sudoers
+    echo "$current_user ALL=(ALL) NOPASSWD: /usr/local/bin/pishrink.sh /home/$current_user/backup-raspis/*" >> /etc/sudoers
+    echo "Lines added to sudoers file."
+fi
