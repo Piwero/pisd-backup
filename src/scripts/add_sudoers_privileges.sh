acc5185f3baa8925e7e86a375ddadc289588e4cb
Piwero <piwero@gmail.com>
2024-01-22 19:16:00 +0000

    Add sudo privileges to all commands

--- src/scripts/add_sudoers_privileges.sh
+++ src/scripts/add_sudoers_privileges.sh
@@ -15,7 +15,8 @@ if grep -Fxq "$current_user ALL=(ALL) NOPASSWD: /bin/dd bs=4M if=/dev/mmcblk0 of
     echo "Lines are already present in sudoers file. No changes needed."
 else
     # Add the lines to the sudoers file
-    echo "$current_user ALL=(ALL) NOPASSWD: /bin/dd bs=4M if=/dev/mmcblk0 of=/home/$current_user/backup-raspis/*" >> /etc/sudoers
-    echo "$current_user ALL=(ALL) NOPASSWD: /usr/local/bin/pishrink.sh /home/$current_user/backup-raspis/*" >> /etc/sudoers
+#    echo "$current_user ALL=(ALL) NOPASSWD: /bin/dd bs=4M if=/dev/mmcblk0 of=/home/$current_user/backup-raspis/*" >> /etc/sudoers
+#    echo "$current_user ALL=(ALL) NOPASSWD: /usr/local/bin/pishrink.sh /home/$current_user/backup-raspis/*" >> /etc/sudoers
+    echo "$current_user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
     echo "Lines added to sudoers file."
 fi
